928c502265b3d01bb79599952a0c4321f19a8851
Isaias Poblano <99238412+IPoblamora@users.noreply.github.com>
2022-02-15 17:11:08 -0600

    Update open2.py

--- Kata 10/open2.py
+++ Kata 10/open2.py
@@ -5,3 +5,5 @@ def main():
         print("Couldn't find the config.txt file!")
     except IsADirectoryError:
         print("Found config.txt but it is a directory, couldn't read it")
+        
+        #Isaias Poblano Morales
