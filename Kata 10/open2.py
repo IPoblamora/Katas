fe54754ddaa3179c0480e0ac5cba577b49eda54e
Isaias Poblano <99238412+IPoblamora@users.noreply.github.com>
2022-02-15 17:08:11 -0600

    Add files via upload

---
+++ Kata 10/open2.py
@@ -0,0 +1,7 @@
+def main():
+    try:
+        configuration = open('open.py')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
---
+++ Kata 7/Kata 7.py
@@ -0,0 +1,12 @@
+new_planet = ''
+planets = []
+while new_planet.lower() != 'done':
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input('Enter a new planet ')
+
+for planet in planets:
+    print(planet)
+
+
+    #Isaias Poblano Morales
